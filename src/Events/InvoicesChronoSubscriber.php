d7e063ec5becf9e26a01d66ec66dcad5f743734f
Cedric <c.caudron59@gmail.com>
2020-02-18 16:51:33 +0100

    Video intervenir creation invoice chrono probleme

---
+++ src/Events/InvoicesChronoSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class InvoicesChronoSubscriber
+ * @package App\Events
+ */
+class InvoicesChronoSubscriber implements EventSubscriberInterface {
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * ['eventName' => 'methodName']
+     *  * ['eventName' => ['methodName', $priority]]
+     *  * ['eventName' => [['methodName1', $priority], ['methodName2']]]
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => 'setChronoForInvoices', EventPriorities::PRE_VALIDATE
+        ];
+    }
+
+    /**
+     * @param ViewEvent $event
+     */
+    public function setChronoForInvoices(ViewEvent $event): void
+    {
+        dd($event->getControllerResult());
+    }
+}
